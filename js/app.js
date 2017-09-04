b0a6eb0a84eb82b1c1e1692db6fff6b37c0b45b1
Andrew Wang <me@andrewwang.ca>
2017-09-04 16:41:24 -0400

    Support prerequisites that have a list of str and list of list in json

--- js/app.js
+++ js/app.js
@@ -3,21 +3,28 @@ $(document).foundation();
 var elements;
 var courses;
 var selected = [];
+var hovering;
 
 function meetPre(course) {
-  var pre = courses[course].pre;
-  for (var i = 0; i < pre.length; i++) {
-    var nextBucket = pre[i];
+  var prerequisite = courses[course].prerequisite ?
+                     courses[course].prerequisite.list :
+                     [];
+  for (var i = 0; i < prerequisite.length; i++) {
+    var nextBucket = prerequisite[i];
     var found = false;
-
-    for (var j = 0; j < nextBucket.length; j++) {
-      var nextCourse = nextBucket[j];
-      if (selected.indexOf(nextCourse) > -1) {
+    if (nextBucket.forEach) {
+      for (var j = 0; j < nextBucket.length; j++) {
+        var nextCourse = nextBucket[j];
+        if (selected.indexOf(nextCourse) !== -1) {
+          found = true;
+          break;
+        }
+      }
+    } else {
+      if (selected.indexOf(nextBucket) !== -1) {
         found = true;
-        break;
       }
     }
-
     if (!found) {
       return false;
     }
@@ -26,11 +33,13 @@ function meetPre(course) {
 }
 
 function meetEx(course) {
-  var ex = courses[course].ex;
+  var exclusions = courses[course].exclusions ?
+                   courses[course].exclusions :
+                   [];
   var result = true;
-  for (var i = 0; i < ex.length; i++) {
-    var nextCourse = ex[i];
-    if (selected.indexOf(nextCourse) > -1) {
+  for (var i = 0; i < exclusions.length; i++) {
+    var nextCourse = exclusions[i];
+    if (selected.indexOf(nextCourse) !== -1) {
       result = false;
     }
   }
@@ -44,8 +53,11 @@ function canTakeCourse(course) {
 function isPreOfSelectedCourse(course) {
   for (var i = 0; i < selected.length; i++) {
     var nextSelected = selected[i];
-    for (var j = 0; j < courses[nextSelected].pre.length; j++) {
-      var nextBucket = courses[nextSelected].pre[j];
+    var prerequisite = courses[nextSelected].prerequisite ?
+                       courses[nextSelected].prerequisite.list :
+                       [];
+    for (var j = 0; j < prerequisite.length; j++) {
+      var nextBucket = courses[nextSelected].prerequisite.list[j];
       for (var k = 0; k < nextBucket.length; k++) {
         var nextCourse = nextBucket[k];
         if (nextCourse === course) {
@@ -59,15 +71,17 @@ function isPreOfSelectedCourse(course) {
 
 function getClassName(course) {
   var result;
-  if (selected.indexOf(course) > -1) {
+  if (selected.indexOf(course) !== -1) {
     result = 'callout success';
     if (isPreOfSelectedCourse(course)) {
       result += ' disabled';
     }
-  } else if (canTakeCourse(course)) {
-    result = 'callout primary';
   } else {
-    result = 'callout secondary disabled';
+    if (canTakeCourse(course)) {
+      result = 'callout primary';
+    } else {
+      result = 'callout secondary disabled';
+    }
   }
   return result;
 }
@@ -82,14 +96,14 @@ function refreshCourseStatus() {
 function toggleCourse() {
   var course = this.id;
   var className = this.className;
-  if (className.indexOf('disabled') > -1) {
+  if (className.indexOf('disabled') !== -1) {
     return;
   }
-  if (className.indexOf('primary') > -1) {
+  if (className.indexOf('primary') !== -1) {
     selected.push(course);
-  } else if (className.indexOf('success') > -1) {
+  } else if (className.indexOf('success') !== -1) {
     var index = selected.indexOf(course);
-    if (index > -1) {
+    if (index !== -1) {
       selected.splice(index, 1);
     }
   }
@@ -100,35 +114,58 @@ function populateHTML() {
   var html = '';
   var count = 0;
   Object.keys(courses).forEach(function(nextCourse) {
-    var title = courses[nextCourse].title;
-    var preList = [];
-    courses[nextCourse].pre.forEach(function(nextOrGroup) {
-      preList.push('[' + nextOrGroup.join(' or ') + ']');
-    });
-    var pre = preList.join(' and ');
-    var ex = courses[nextCourse].ex.join(', ');
+    if (nextCourse.indexOf('CSC') !== -1) {
+      var title = courses[nextCourse].title;
+      var preList = [];
+      var prerequisite;
+      var exclusions;
+      console.log(courses[nextCourse]);
+      if (courses[nextCourse].prerequisite) {
+        courses[nextCourse].prerequisite.list.forEach(function(nextOrGroup) {
+          if (nextOrGroup.forEach) {
+            preList.push('[' + nextOrGroup.join(' or ') + ']');
+          } else {
+            preList.push(nextOrGroup);
+          }
+        });
+        prerequisite = preList.join(' and ');
+      }
+      if (courses[nextCourse].exclusions) {
+        exclusions = courses[nextCourse].exclusions.join(', ');
+      }
 
-    if (count === 0) {
-      html += '<div class="row">';
-    } else if (count % 4 === 0) {
-      html += '</div><div class="row">';
+      if (count === 0) {
+        html += '<div class="row">';
+      } else if (count % 4 === 0) {
+        html += '</div><div class="row">';
+      }
+      html += '<div class="large-3 columns"><div id="' + nextCourse;
+      html += '" class="callout"><h5>' + nextCourse;
+      html += '</h5><p>' + title;
+      html += '</p><p><b>Prerequisite:&nbsp;</b>' + (prerequisite ? prerequisite : 'None');
+      html += '</p><p><b>Exclusion:&nbsp;</b>' + (exclusions ? exclusions : 'None');
+      html += '</p></div></div>';
+      count++;
     }
-    html += '<div class="large-3 columns"><div id="' + nextCourse;
-    html += '" class="callout"><h5>' + nextCourse;
-    html += '</h5><p>' + title;
-    html += '</p><p><b>Prerequisite:&nbsp;</b>' + (pre ? pre : 'None');
-    html += '</p><p><b>Exclusion:&nbsp;</b>' + (ex ? ex : 'None');
-    html += '</p></div></div>';
-    count++;
   });
   html += '</div>';
   document.getElementById('courses-canvas').innerHTML = html;
 }
 
-$.getJSON('./courses.json', function(data) {
+function mouseIn() {
+  var hovered = this.id;
+  refreshCourseStatus();
+}
+
+function mouseOut() {
+  console.log('out');
+}
+
+$.getJSON('./courses/C.json', function(data) {
   courses = data;
   populateHTML();
   elements = $('.callout');
   elements.click(toggleCourse);
+  // elements.hover(mouseIn, mouseOut);
   refreshCourseStatus();
 });
